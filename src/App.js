116b47bfa41ad0e8e3a89f1a78ce1eb2984f70bf
Bryant Vaughn <bf_vaughn@hotmail.com>
2020-03-21 19:34:14 -0600

    list of dogs and each dog details item complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DogList from './DogList';
+import DogDetails from './DogDetails';
+import Navbar from './Navbar';
+import { Switch, Route } from 'react-router-dom';
+import whiskey from './images/whiskey.jpg';
+import hazel from './images/hazel.jpg';
+import tubby from './images/tubby.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <DogList />
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whickey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog.",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating treats!"
+        ]
+      }
+    ]
+  };
+
+  render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={ currentDog } />;
+    }
+    return (
+      <div>
+        <Navbar dogs={ this.props.dogs } />
+        <Switch>
+          <Route exact path="/dogs" render={ () => <DogList dogs={ this.props.dogs } />} />
+          <Route exact path="/dogs/:name" render={ getDog } />
+        </Switch>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
